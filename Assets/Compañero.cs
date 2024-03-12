01d048487b22c3d607e94d7ea672d1f8fda2dced
Luis <142854556+AstroLui@users.noreply.github.com>
2024-03-12 09:52:55 -0400

    feat: adding the ataque for Compas

--- Assets/Compañero.cs
+++ Assets/Compañero.cs
@@ -1,40 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Compañero : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject Object;
     public float velocidad;
     SpriteRenderer spriteRenderer;
+    Animator animator;
     public float compañeroYPosition;
+
+    public float detectionDistance = 10f;
+    public bool hasTargetedEnemy = false;
+    public bool detectionIsActived = true;
+    private Transform enemyTransform;
     void Start()
     {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        FlipSprite();
-        seguirPlayer();
+        FollowPlayer();
+        HabilidadHasActived();
     }
 
-    void FlipSprite(){
-
-        if(player.transform.position.x > transform.position.x){
-            spriteRenderer.flipX = false;
-    } else if(player.transform.position.x < transform.position.x)
+    void HabilidadHasActived()
     {
-        spriteRenderer.flipX = true;
+        if (Input.GetButtonDown("Fire2"))
+        {
+            animator.SetTrigger("HabilidadHasActived");
+        }
     }
-}
-    void seguirPlayer(){
+    private void FlipSprite(string Direction)
+    {
 
-        float distanciaPlayerX = player.transform.position.x - transform.position.x;
-        float distanciaPlayerY = player.transform.position.y - transform.position.y;
-        if(distanciaPlayerX < 2 && distanciaPlayerX > -2) return;
-        if(distanciaPlayerY < 1 && distanciaPlayerY > 1)  return;
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, velocidad * Time.deltaTime);
+        if (Direction == "right")
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (Direction == "left")
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+    void FollowPlayer()
+    {
+        if (!hasTargetedEnemy)
+        {
+            if (Object.transform.position.x > transform.position.x)
+            {
+                FlipSprite("right");
+            }
+            else if (Object.transform.position.x < transform.position.x)
+            {
+                FlipSprite("left");
+            }
+            transform.position = Vector2.MoveTowards(transform.position, Object.transform.position, velocidad * Time.deltaTime);
+        }
+    }
+    IEnumerator CoolDownDetection(float seconds)
+    {
+        detectionIsActived = false;
+        yield return new WaitForSeconds(seconds);
+        detectionIsActived = true;
+    }
+    private void OnTriggerStay2D(Collider2D Other)
+    {
+        int EnemyLayer = LayerMask.NameToLayer("Enemigos");
+
+        if (Other.gameObject.layer == EnemyLayer && detectionIsActived)
+        {
+            enemyTransform = Other.transform;
+            hasTargetedEnemy = true;
+            float distancia = Vector2.Distance(transform.position, enemyTransform.position);
+            if (distancia < detectionDistance)
+            {
+                FlipSprite(enemyTransform.position.x > transform.position.x ? "right" : "left");
+                transform.position = Vector2.MoveTowards(transform.position, enemyTransform.position, velocidad * Time.deltaTime);
+            }
+            if (distancia < 2)
+            {
+                animator.SetTrigger("HabilidadHasActived");
+            }
+            if (distancia < 0.3)
+            {
+                Destroy(Other.gameObject);
+                hasTargetedEnemy = false;
+                StartCoroutine(CoolDownDetection(10f));
+            }
+        }
+        
     }
 }
