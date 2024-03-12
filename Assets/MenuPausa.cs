855a6d97fd0ae675446307fe30ecbca6fc207492
MoisesLondo <moiseslondo16@gmail.com>
2024-03-12 13:15:55 -0400

    menu

---
+++ Assets/MenuPausa.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    public static bool juegoPausado = false;
+    public GameObject menuPausaUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.Escape))
+
+     if (juegoPausado)
+     {
+        Reanudar();
+     }
+     else{
+        Pausa();
+     }
+
+    }
+
+    void Reanudar(){
+
+        menuPausaUI.SetActive(false);
+        Time.timeScale = 1f;
+        juegoPausado = false;
+    }
+
+    void Pausa(){
+
+        menuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+        juegoPausado = true;
+    }
+}
